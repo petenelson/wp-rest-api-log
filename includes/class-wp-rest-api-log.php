002006af83f5b8fb4cf12245dc6ab3cc5ab18549
Pete Nelson <pete@petenelson.com>
2015-07-03 17:25:04 -0500

    added filter to allow bypassing of logging for specific requests

--- includes/class-wp-rest-api-log.php
+++ includes/class-wp-rest-api-log.php
@@ -12,10 +12,29 @@ if ( ! class_exists( 'WP_REST_API_Log' ) ) {
 			// filter that is called by the REST API right before it sends a response
 			add_filter( 'rest_pre_serve_request', array( $this, 'log_rest_api_dispatch' ), 10, 4 );
 
+			add_filter( 'wp-rest-api-log-bypass-insert', function( $bypass_insert, $result, $request, $rest_server ) {
+				// an example of disabling logging for specific requests
+
+				// other conditions can be checked here
+				if ( '/hello/world' === $request->get_route() ) {
+					$bypass_insert = true;
+				}
+
+				return $bypass_insert;
+
+			}, 10, 4 );
+
 		}
 
 
 		public function log_rest_api_dispatch( $served, $result, $request, $rest_server ) {
+
+			// allow specific requests to not be logged
+			$bypass_insert = apply_filters( WP_REST_API_Log_Common::$plugin_name . '-bypass-insert', false, $result, $request, $rest_server );
+			if ( $bypass_insert ) {
+				return $served;
+			}
+
 
 			$args = array(
 				'ip_address'            => $_SERVER['REMOTE_ADDR'],
