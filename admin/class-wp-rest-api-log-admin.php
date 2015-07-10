f468620580c290e957dd08b12bd8f1fc39cfc5f2
Pete Nelson <pete@petenelson.com>
2015-07-09 19:27:40 -0500

    added check in admin to make sure log is enabled

--- admin/class-wp-rest-api-log-admin.php
+++ admin/class-wp-rest-api-log-admin.php
@@ -25,6 +25,12 @@ if ( ! class_exists( 'WP_REST_API_Log_Admin' ) ) {
 
 		public function display_entries() {
 
+			if ( ! WP_REST_API_Log_Common::api_is_enabled() ) {
+				require_once dirname( __FILE__ ) . '/partials/wp-rest-api-log-api-is-disabled.php';
+				return;
+			}
+
+
 			global $wp_rest_api_log_display_entries;
 
 			$db = new WP_REST_API_Log_DB_Entries();
--- includes/class-wp-rest-api-log-common.php
+++ includes/class-wp-rest-api-log-common.php
@@ -9,14 +9,22 @@ if ( ! class_exists( 'WP_REST_API_Log_Common' ) ) {
 		static $plugin_name    = 'wp-rest-api-log';
 		static $version        = '2015-07-03-01';
 
+		const TEXT_DOMAIN      = 'wp-rest-api-log';
+
 
 		public function plugins_loaded() {
 
 		}
 
+
 		static public function current_milliseconds() {
 			list( $usec, $sec ) = explode( " ", microtime() );
 			return ( ( (float)$usec + (float)$sec ) ) * 1000;
+		}
+
+
+		static public function api_is_enabled() {
+			return class_exists( 'WP_REST_Server' ) && apply_filters( 'rest_enabled', true );
 		}
 
 
