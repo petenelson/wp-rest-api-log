8c2e4cb9e91b564dfa98cbcc342e1c7de8400476
pete@petenelson.com <petenelson@fortress.local>
2016-03-21 20:52:03 -0500

    removed edit from bulk actions

--- admin/class-wp-rest-api-log-admin-list-table.php
+++ admin/class-wp-rest-api-log-admin-list-table.php
@@ -11,15 +11,19 @@ if ( ! class_exists( 'WP_REST_API_Log_Admin_List_Table' ) ) {
 
 		public function plugins_loaded() {
 			add_action( 'admin_init', array( $this, 'admin_init' ) );
-			add_action( 'restrict_manage_posts', array( $this, 'add_method_dropdown' ) );
-			add_action( 'restrict_manage_posts', array( $this, 'add_status_dropdown' ) );
 		}
 
 
 		public function admin_init() {
-			add_filter( 'post_row_actions', array( $this, 'post_row_actions' ), 10, 2 );
-			add_filter( 'manage_edit-' . WP_REST_API_Log_Db::POST_TYPE . '_columns' , array( $this, 'custom_columns' ) );
-			add_action( 'manage_' . WP_REST_API_Log_Db::POST_TYPE . '_posts_custom_column',  array( $this, 'custom_column' ), 10, 2 );
+			$post_type = WP_REST_API_Log_Db::POST_TYPE;
+
+			add_filter( 'post_row_actions',                          array( $this, 'post_row_actions' ), 10, 2 );
+			add_filter( "manage_edit-{$post_type}_columns" ,         array( $this, 'custom_columns' ) );
+			add_action( "manage_{$post_type}_posts_custom_column",   array( $this, 'custom_column' ), 10, 2 );
+			add_filter( "bulk_actions-edit-{$post_type}",            array( $this, 'remove_edit_bulk_action' ) ) ;
+
+			add_action( 'restrict_manage_posts',                     array( $this, 'add_method_dropdown' ) );
+			add_action( 'restrict_manage_posts',                     array( $this, 'add_status_dropdown' ) );
 		}
 
 
@@ -130,6 +134,10 @@ if ( ! class_exists( 'WP_REST_API_Log_Admin_List_Table' ) ) {
 			return $this->_post;
 		}
 
+		public function remove_edit_bulk_action( $actions ) {
+			unset( $actions['edit'] );
+			return $actions;
+		}
 
 	}
 
