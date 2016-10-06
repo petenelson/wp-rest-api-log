b0222ecb09e9b1aabd2a3b2ad389ae7564ca9609
Pete Nelson <pete@petenelson.com>
2016-10-06 14:16:53 -0500

    updated highlight_version to 9.7.0, updated readme

--- wp-rest-api-log.php
+++ wp-rest-api-log.php
@@ -3,7 +3,7 @@
  * Plugin Name: REST API Log
  * Description: Logs requests and responses for the REST API
  * Author: Pete Nelson
- * Version: 1.4.0
+ * Version: 1.3.0
  * Plugin URI: https://github.com/petenelson/wp-rest-api-log
  * Text Domain: wp-rest-api-log
  * Domain Path: /languages
