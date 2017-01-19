937c8d3b16a4ecd701d3498ef7a6979ee898bc1c
Pete Nelson <pete@petenelson.com>
2017-01-18 19:43:22 -0600

    finished can_log_route()

--- includes/class-wp-rest-api-log-filters.php
+++ includes/class-wp-rest-api-log-filters.php
@@ -48,6 +48,9 @@ class WP_REST_API_Log_Filters {
 
 				// Add the end of the match.
 				$route_filter .= '$';
+
+				// Convert backslash to literals.
+				$route_filter = str_replace( '/', "\/", $route_filter );
 			}
 
 		}
@@ -55,11 +58,53 @@ class WP_REST_API_Log_Filters {
 		return $route_filter;
 	}
 
+	/**
+	 * Determines if the supplied route can be logged.
+	 *
+	 * @param  string $route Route (ex: /wp/v2).
+	 * @return bool
+	 */
 	static public function can_log_route( $route ) {
 
 		// Get the filter mode.
+		$route_logging_mode = apply_filters( 'wp-rest-api-log-setting-get', 'routes', 'route-log-matching-mode' );
+
+		// If no logging mode is set, we can log the route.
+		if ( empty( $route_logging_mode ) ) {
+			return true;
+		}
 
 		// Get the route filters.
+		$route_filters = apply_filters( 'wp-rest-api-log-setting-get', 'routes', 'route-filters' );
+		$route_filters = explode( "\n", $route_filters );
 
+		// If we're set to exclude matching filters, but we have no filters,
+		// then the route can be logged
+		if ( 'exclude_matches' === $route_logging_mode && empty( $route_filters ) ) {
+			return true;
+		}
+
+		// Loop through the filters and apply each one to the route.
+		foreach( $route_filters as $route_filter ) {
+			$regex = self::route_to_regex( $route_filter );
+
+			//preg_match() returns 1 if the pattern matches given subject,
+			//0 if it does not, or FALSE if an error occurred.
+			$match = preg_match( '/' . $regex . '/', $route );
+
+			// We can log this if the mode is set to log only matches.
+			if ( 1 === $match && 'log_matches' === $route_logging_mode ) {
+				return true;
+			}
+
+			// We cannot log this if the mode is set to exclude matches.
+			if ( 1 === $match && 'exclude_matches' === $route_logging_mode ) {
+				return false;
+			}
+		}
+
+		// At this point, we can only log the match if we're set to exclude
+		// mode and the loop above did not find a match.
+		return 'exclude_matches' === $route_logging_mode;
 	}
 }
--- tests/test-filters.php
+++ tests/test-filters.php
@@ -15,14 +15,14 @@ class WP_REST_API_Log_Test_Filters extends WP_UnitTestCase {
 
 		// Just a path, adds start and end.
 		$route_regex = WP_REST_API_Log_Filters::route_to_regex( '/wp/v2/' );
-		$this->assertEquals( '^/wp/v2/?$', $route_regex );
+		$this->assertEquals( "^\/wp\/v2\/?$", $route_regex );
 
 		// Wildcard matches.
 		$route_regex = WP_REST_API_Log_Filters::route_to_regex( '/wp/*/' );
-		$this->assertEquals( '^/wp/.*/?$', $route_regex );
+		$this->assertEquals( "^\/wp\/.*\/?$", $route_regex );
 
 		$route_regex = WP_REST_API_Log_Filters::route_to_regex( '/wp/v2/*' );
-		$this->assertEquals( '^/wp/v2/.*/?$', $route_regex );
+		$this->assertEquals( "^\/wp\/v2\/.*\/?$", $route_regex );
 
 		// Regex should have no changes.
 		$route_regex = WP_REST_API_Log_Filters::route_to_regex( '^/wp/v2/.*' );
@@ -30,7 +30,7 @@ class WP_REST_API_Log_Test_Filters extends WP_UnitTestCase {
 		
 		// This is not treated as regex, so it get mangled.
 		$route_regex = WP_REST_API_Log_Filters::route_to_regex( '.*/wp/v2/$' );
-		$this->assertEquals( '^..*/wp/v2/$/?$', $route_regex );
+		$this->assertEquals( "^..*\/wp\/v2\/$\/?$", $route_regex );
 	}
 
 	public function test_filter_modes() {
@@ -40,9 +40,96 @@ class WP_REST_API_Log_Test_Filters extends WP_UnitTestCase {
 		$this->assertArrayHasKey( 'exclude_matches', $modes );
 	}
 
-	public function test_should_log_route() {
+	public function test_route_logging_all_routes() {
+
+		$option_key = 'wp-rest-api-log-settings-routes';
+
+		// Set the options to log everything.
+		$option_value = array(
+			'route-log-matching-mode' => '',
+			);
+
+		update_option( 'wp-rest-api-log-settings-routes', $option_value );
+
+		// Make sure we can log any route.
+		$this->assertTrue( WP_REST_API_Log_Filters::can_log_route( '/wp/v2' ) );
+		$this->assertTrue( WP_REST_API_Log_Filters::can_log_route( '/wp/v2/posts' ) );
+		$this->assertTrue( WP_REST_API_Log_Filters::can_log_route( '/wp/v2/users' ) );
+
+	}
 
 
+	public function test_route_logging_only_matched_routes() {
+
+		$option_key = 'wp-rest-api-log-settings-routes';
+
+		// Set the options to log only matching routes.
+		$option_value = array(
+			'route-log-matching-mode' => 'log_matches',
+			'route-filters' =>
+"/wp/v2
+/route/wildcard*
+^\/route\/regex-exact$
+^\/route\/regex-wildcard.*$"
+			);
+
+		update_option( 'wp-rest-api-log-settings-routes', $option_value );
+
+		// Exact match.
+		$this->assertTrue( WP_REST_API_Log_Filters::can_log_route( '/wp/v2' ), '/wp/v2' );
+
+		// Basic wildcard.
+		$this->assertTrue( WP_REST_API_Log_Filters::can_log_route( '/route/wildcard-route' ), '/route/wildcard-route' );
+
+		// Exact regex
+		$this->assertTrue( WP_REST_API_Log_Filters::can_log_route( '/route/regex-exact' ), '/route/regex-exact' );
+
+		// Wildcard regex
+		$this->assertTrue( WP_REST_API_Log_Filters::can_log_route( '/route/regex-wildcard/test' ), '/route/regex-wildcard/test' );
+
+		// Test non-matching routes.
+		$this->assertFalse( WP_REST_API_Log_Filters::can_log_route( '/some/route' ), '/some/route' );
+		$this->assertFalse( WP_REST_API_Log_Filters::can_log_route( '/route/wildercard' ), '/route/wildercard' );
+		$this->assertFalse( WP_REST_API_Log_Filters::can_log_route( '/route/exact-regex' ), '/route/exact-regex' );
+		$this->assertFalse( WP_REST_API_Log_Filters::can_log_route( '/route/regexwildcard' ), '/route/regexwildcard' );
+		$this->assertFalse( WP_REST_API_Log_Filters::can_log_route( '/wp/v2/posts' ), '/wp/v2/posts' );
+
+	}
+
+	public function test_route_logging_excluded_matched_routes() {
+
+		$option_key = 'wp-rest-api-log-settings-routes';
+
+		// Set the options to log only matching routes.
+		$option_value = array(
+			'route-log-matching-mode' => 'exclude_matches',
+			'route-filters' =>
+"/wp/v2
+/route/wildcard*
+^\/route\/regex-exact$
+^\/route\/regex-wildcard.*$"
+			);
+
+		update_option( 'wp-rest-api-log-settings-routes', $option_value );
+
+		// Exact match.
+		$this->assertFalse( WP_REST_API_Log_Filters::can_log_route( '/wp/v2' ), '/wp/v2' );
+
+		// Basic wildcard.
+		$this->assertFalse( WP_REST_API_Log_Filters::can_log_route( '/route/wildcard-route' ), '/route/wildcard-route' );
+
+		// Exact regex
+		$this->assertFalse( WP_REST_API_Log_Filters::can_log_route( '/route/regex-exact' ), '/route/regex-exact' );
+
+		// Wildcard regex
+		$this->assertFalse( WP_REST_API_Log_Filters::can_log_route( '/route/regex-wildcard/test' ), '/route/regex-wildcard/test' );
+
+		// Test non-matching routes, should all be logged.
+		$this->assertTrue( WP_REST_API_Log_Filters::can_log_route( '/some/route' ), '/some/route' );
+		$this->assertTrue( WP_REST_API_Log_Filters::can_log_route( '/route/wildercard' ), '/route/wildercard' );
+		$this->assertTrue( WP_REST_API_Log_Filters::can_log_route( '/route/exact-regex' ), '/route/exact-regex' );
+		$this->assertTrue( WP_REST_API_Log_Filters::can_log_route( '/route/regexwildcard' ), '/route/regexwildcard' );
+		$this->assertTrue( WP_REST_API_Log_Filters::can_log_route( '/wp/v2/posts' ), '/wp/v2/posts' );
 
 	}
 
