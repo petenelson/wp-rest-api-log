a84b3a2e5179b3e85238277a47a603fbfc2429bd
Pete Nelson <pete@petenelson.com>
2017-01-20 18:31:03 -0600

    fixed some route tests

--- tests/test-filters.php
+++ tests/test-filters.php
@@ -14,15 +14,15 @@ class WP_REST_API_Log_Test_Filters extends WP_UnitTestCase {
 	public function test_convert_route_filter() {
 
 		// Just a path, adds start and end.
-		$route_regex = WP_REST_API_Log_Filters::route_to_regex( '/wp/v2/' );
+		$route_regex = WP_REST_API_Log_Filters::route_to_regex( '/wp/v2' );
 		$this->assertEquals( "^\/wp\/v2$", $route_regex );
 
 		// Wildcard matches.
-		$route_regex = WP_REST_API_Log_Filters::route_to_regex( '/wp/*/' );
+		$route_regex = WP_REST_API_Log_Filters::route_to_regex( '/wp/*' );
 		$this->assertEquals( "^\/wp\/.*$", $route_regex );
 
 		$route_regex = WP_REST_API_Log_Filters::route_to_regex( '/wp/v2/*' );
-		$this->assertEquals( "^\/wp\/v2\/.*$", $route_regex );
+		$this->assertEquals( "^\/wp\/v2\/.*$", $route_regex, '/wp/v2/*' );
 
 		// Regex should have no changes.
 		$route_regex = WP_REST_API_Log_Filters::route_to_regex( '^/wp/v2/.*' );
@@ -30,7 +30,7 @@ class WP_REST_API_Log_Test_Filters extends WP_UnitTestCase {
 		
 		// This is not treated as regex, so it get mangled.
 		$route_regex = WP_REST_API_Log_Filters::route_to_regex( '.*/wp/v2/$' );
-		$this->assertEquals( "^..*\/wp\/v2$", $route_regex );
+		$this->assertEquals( "^..*\/wp\/v2\/$$", $route_regex );
 	}
 
 	public function test_filter_modes() {
