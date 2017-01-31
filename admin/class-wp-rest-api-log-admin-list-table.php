54550ee54065216c0866b438dd33eb9caf28a418
Drsdre <github.com@drsdre.org>
2017-01-31 16:19:19 +0100

    Add user to overview

--- admin/class-wp-rest-api-log-admin-list-table.php
+++ admin/class-wp-rest-api-log-admin-list-table.php
@@ -60,6 +60,7 @@ if ( ! class_exists( 'WP_REST_API_Log_Admin_List_Table' ) ) {
 				'elapsed'    => __( 'Elapsed Time', 'wp-rest-api-log' ),
 				'length'     => __( 'Response Length', 'wp-rest-api-log' ),
 				'ip-address' => __( 'IP Address', 'wp-rest-api-log' ),
+				'user'       => __( 'User', 'wp-rest-api-log' ),
 				);
 
 
