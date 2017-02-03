f79d98ff632afcf7d58a03d4ed7f621bd7b412dc
Pete Nelson <pete@petenelson.com>
2017-02-02 19:23:42 -0600

    added link to Settings from plugins list

--- admin/class-wp-rest-api-log-admin.php
+++ admin/class-wp-rest-api-log-admin.php
@@ -21,6 +21,7 @@ if ( ! class_exists( 'WP_REST_API_Log_Admin' ) ) {
 
 			add_filter( 'user_has_cap', 'WP_REST_API_Log_Admin::add_admin_caps', 10, 3 );
 
+			add_filter( 'plugin_action_links_' . WP_REST_API_LOG_BASENAME, array( __CLASS__, 'plugin_action_links' ), 10, 4 );
 		}
 
 
@@ -167,7 +168,39 @@ if ( ! class_exists( 'WP_REST_API_Log_Admin' ) ) {
 			return $allcaps;
 		}
 
+		/**
+		 * Adds additional links to the row on the plugins page.
+		 *
+		 * @param array  $actions     An array of plugin action links.
+		 * @param string $plugin_file Path to the plugin file relative to the plugins directory.
+		 * @param array  $plugin_data An array of plugin data.
+		 * @param string $context     The plugin context. Defaults are 'All', 'Active',
+		 *                            'Inactive', 'Recently Activated', 'Upgrade',
+		 *                            'Must-Use', 'Drop-ins', 'Search'.
+		 */
+		static public function plugin_action_links( $actions, $plugin_file, $plugin_data, $context ) {
 
+			if ( is_plugin_active( $plugin_file ) && current_user_can( 'manage_options' ) ) {
+
+				// Build the URL for the settings page.
+				$url = add_query_arg(
+					'page',
+					rawurlencode( WP_REST_API_Log_Settings::$settings_page ),
+					admin_url( 'admin.php' )
+					);
+
+				// Add the anchor tag to the list of plugin links.
+				$new_actions = array(
+					'settings' => sprintf( '<a href="%1$s">%2$s</a>',
+						esc_url( $url ),
+						esc_html__( 'Settings' )
+						)
+					);
+
+				$actions = array_merge( $new_actions, $actions );
+			}
+
+			return $actions;
+		}
 	}
-
 }
